cb2235be39a463e3d928af19d7f55b6abaf4443a
kentaro123 <kentaro.w0918@gmail.com>
2020-12-03 21:52:01 +0900

    second

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+import flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello, world'
+    return flask.render_template('login.html')
+
+@app.route('/result',methods=["POST","GET"])
+def look_up():
+    client_params = request.form
+    return flask.render_template("result.html",cont=client_params['entry1'])
 
 if __name__ == '__main__':
     app.run()
